4d59d64b9c5f09ed4e0c03d0bb23c9c76bfb047f
Olamide Oladinni <olajide1987@gmail.com>
2020-03-26 23:49:02 -0500

    authwip

--- src/javascripts/helpers/data/authData.js
+++ src/javascripts/helpers/data/authData.js
@@ -1,2 +1,21 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+
+const authDiv = $('#auth');
+const logoutButton = $('#navbar-logout-button');
+
+const checkLoginStatus = () => {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      // person is logged in
+      authDiv.addClass('hide');
+      logoutButton.removeClass('hide');
+    } else {
+      // person is NOT logged in
+      authDiv.removeClass('hide');
+      logoutButton.addClass('hide');
+    }
+  });
+};
+
+export default { checkLoginStatus };
