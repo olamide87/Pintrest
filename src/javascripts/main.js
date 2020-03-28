69c1a3cb89182ff57bdc1586f16355cfb076ac24
Olamide Oladinni <olajide1987@gmail.com>
2020-03-28 11:41:21 -0500

    add seed data

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase/app';
-import apiKeys from './helpers/apiKeys.example.json';
+import apiKeys from './helpers/apikeys.json';
 import authData from './helpers/data/authData';
 import auth from './components/auth/auth';
 import myNavbar from './components/myNavbar/myNavbar';
 import '../styles/main.scss';
 
 const init = () => {
-  firebase.initializeApp(apiKeys.firebaseKeys);
+  firebase.initializeApp(apiKeys.firebaseConfig);
   authData.checkLoginStatus();
   auth.loginButton();
   myNavbar.logoutEvent();
