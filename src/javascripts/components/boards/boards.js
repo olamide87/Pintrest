31fd83031aa4b170659afb851c23a1d159058faf
Olamide Oladinni <olajide1987@gmail.com>
2020-04-01 00:50:25 -0500

    finish board printing

--- src/javascripts/components/boards/boards.js
+++ src/javascripts/components/boards/boards.js
@@ -1,17 +1,120 @@
-// need to write code that prints 4 different boards(cards) to the page 4 different div containers of equal size
-// when you click on one of the boards it should hide the other three boards and only display the selected board
-// when your you have single board view you should be able to see all the pins on the boards 
+
 import './boards.scss';
 import $ from 'jquery';
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import utilities from '../../helpers/utilities';
+import utils from '../../helpers/utils';
 import boardData from '../../helpers/data/boardData';
-import boardCard from '../BoardCard/boardCard';
+import boardCard from '../boardCard/boardCard';
 import pinsData from '../../helpers/data/pinData';
-import pinsPrint from '../PinCard/pinCard';
+import pinsPrint from '../pinCard/pinCard';
 
 
-const boardsBuilder = () => {
+const updatePinHandler = (e) => {
+  const pinId = e.target.id.split('pin-')[1];
+  $('saveUpdatePinButton').attr('id', `updatePin-${pinId}`);
+};
 
-}
+
+const deleteABoard = (e) => {
+  e.preventDefault();
+  const { uid } = firebase.auth().currentUser;
+  const boardId = e.target.id.split('board-')[1];
+  boardData.deleteBoard(boardId)
+    .then(() => {
+      pinsData.getPinsByBoardId(boardId).then((pins) => {
+        pins.forEach((pin) => pinsData.deletePin(pin.id));
+      });
+      // eslint-disable-next-line no-use-before-define
+      buildBoards(uid);
+    })
+    .catch((error) => console.error(error));
+};
+
+const addNewBoard = (e) => {
+  e.stopImmediatePropagation();
+  const { uid } = firebase.auth().currentUser;
+  const newBoard = {
+    name: $('#boardName').val(),
+    boardImg: $('#boardImageUrl').val(),
+    uid,
+  };
+  boardData.addBoard(newBoard)
+    .then(() => {
+      $('#exampleModal').modal('hide');
+      // eslint-disable-next-line no-use-before-define
+      buildBoards(uid);
+    })
+    .catch((error) => console.error(error));
+};
+
+const showSingleBoard = (boardId) => {
+  pinsData.getPinsByBoardId(boardId)
+    .then((pins) => {
+      // console.log('here are the pins', pins);
+      // eslint-disable-next-line max-len
+      let domString = '<div id="boardSection" class="container text-center"><button class="closeBtn">Close</button> <button type="button" id="newPinBtn" class="btn btn-outline-dark" data-toggle="modal" data-target="#pinModal"> Add New Pin</button></div>';
+      domString += '<div class="container d-flex flex-wrap justify-content-between">';
+      pins.forEach((pin) => {
+        domString += pinsPrint.makePin(pin);
+      });
+      domString += '</div>';
+      utils.printToDom('boards', domString);
+      $('#boardSection').on('click', '.closeBtn', close);
+      $('#addNewPin').attr('pinBoardId', boardId);
+      $('.updatePinButton').click(updatePinHandler);
+      $('#updatePinModal').on('click', '.saveUpdatePinButton', updatePin);
+    })
+    .catch((error) => console.error(error));
+};
+
+const deleteAPin = (e) => {
+  e.preventDefault();
+  pinsData.deletePin(e.target.id)
+    .then(() => {
+      const boardId = e.target.getAttribute('pinDataBoardId');
+      showSingleBoard(boardId);
+      // eslint-disable-next-line no-use-before-define
+    })
+    .catch((error) => console.error(error));
+};
+
+const createSingleBoard = (e) => {
+  const boardId = e.target.id;
+  showSingleBoard(boardId);
+};
+
+const buildBoards = (uid) => {
+  boardData.getBoards(uid)
+    .then((boards) => {
+      let domString = `<div class="text-center" style="padding:50px"><button type="button" class="btn btn-outline-dark btn-lg" data-toggle="modal" data-target="#exampleModal">
+      Add Board
+    </button>`;
+      domString += '<div id="boardSection" class="container d-flex flex-wrap justify-content-between">';
+      boards.forEach((board) => {
+        domString += boardCard.makeABoard(board);
+      });
+      let domString2 = '<div>';
+      domString += '</div>';
+      domString2 += '</div>';
+      utils.printToDom('boards', domString);
+      utils.printToDom('updatePinBoard', domString2);
+      $('#boards').on('click', '.chosen-board', createSingleBoard);
+      $('#boards').on('click', '.deletePinFromBoard', deleteAPin);
+      $('#boards').on('click', '.deleteBoard', deleteABoard);
+      $('#addNewBoardBtn').click(addNewBoard);
+      $('#addNewPin').click(addNewPin);
+    });
+};
+
+const close = () => {
+  const { uid } = firebase.auth().currentUser;
+  $(document).click((e) => {
+    const buttonName = e.target.className;
+    if (buttonName === 'closeBtn') {
+      buildBoards(uid)
+    }
+  });
+};
+
+export default { buildBoards };
